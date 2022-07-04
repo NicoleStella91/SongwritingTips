b1c1d907a207b02d29085bdb60c7bcb2c7dab439
Nicole Stella <nicole.stella01@gmail.com>
2022-07-04 18:59:30 +0200

    Better design

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Header accordion menu to browse content
 var acc = document.getElementsByClassName("accordion");
 var i;
 
@@ -12,3 +13,67 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
+
+//Hero slider for Popular Articles
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("slides-container");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length} ;
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "block";
+}
+
+//Slider for Collections
+
+var collectionIndex = 1;
+showCol(collectionIndex);
+
+function plusCol(n) {
+  showCol(collectionIndex += n);
+}
+
+function showCol(n) {
+  var i;
+  var x = document.getElementsByClassName("collection");
+  if (n > x.length) {collectionIndex = 1}
+  if (n < 1) {collectionIndex = x.length} ;
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[collectionIndex-1].style.display = "block";
+}
+
+
+//Collapsible cards
+
+var coll = document.getElementsByClassName("collapsible");
+var n;
+
+for (n = 0; n < coll.length; n++)
+{
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "none") {
+      content.display.style = "none";
+    } else {
+      content.display.style = "block";
+    }
+  });
+}
+
+
+
+
+
